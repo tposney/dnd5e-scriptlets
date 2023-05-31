b8551c4f8947d77b0d9f4d26d3efb9f6c400336b
Tim Posney <tposney@gmail.com>
2023-05-31 15:02:07 +1000

    module rename

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,7 +1,7 @@
 export function registerSettings() {
-game.settings.register("dnd5e_scriptletss", "lukasSheetFilter", {
-  name: game.i18n.localize("dnd5e_scriptletss.LukasSheetFilter.Name"),
-  hint: game.i18n.localize("dnd5e_scriptletss.LukasSheetFilter.Hint"),
+game.settings.register("dnd5e_scriptlets", "lukasSheetFilter", {
+  name: game.i18n.localize("dnd5e_scriptlets.LukasSheetFilter.Name"),
+  hint: game.i18n.localize("dnd5e_scriptlets.LukasSheetFilter.Hint"),
   scope: "world",
   default: false,
   config: true,
