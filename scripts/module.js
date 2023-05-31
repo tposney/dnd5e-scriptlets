0eec2fc395b1fd8e30c50f2c84590c8c829828c7
Tim Posney <tposney@gmail.com>
2023-05-31 14:53:44 +1000

    added some logging

--- scripts/module.js
+++ scripts/module.js
@@ -2,9 +2,13 @@ import { addLukasCharSheetFilter } from "./lukas-char-sheet-qol.js";
 import { registerSettings } from "./settings.js";
 
 Hooks.once('init', async function() {
+  console.log("dnd5e_scriptlets | registering settings");
   registerSettings();
 });
 
 Hooks.once('ready', async function() {
-  if (game.settings.get("dnd5e_scriptlets", "lukasSheetFilter")) addLukasCharSheetFilter();
+  if (game.settings.get("dnd5e_scriptlets", "lukasSheetFilter")) {
+    console.log("dnd5e_scriptlets | registering Lukas Character Sheet Filters");
+    addLukasCharSheetFilter();
+  }
 });
